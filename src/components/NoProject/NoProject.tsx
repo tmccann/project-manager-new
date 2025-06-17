4006bfeac1f7a71f7a2bb2dcd3b23805dca45a16
Tommy <tommy.mccann001@gmail.com>
2025-06-17 19:37:53 +0100

    feat: noProject scaffolding and pseudocode

--- src/components/NoProject/NoProject.tsx
+++ src/components/NoProject/NoProject.tsx
@@ -1,5 +1,21 @@
 const NoProject = () => {
-  return <div>NoProject</div>;
+  return (
+    <div>
+      NoProject
+      {/* IF projects exist THEN
+          Display message: "No project selected"
+          Display message: "Select a project"
+          Display message: "or"
+          Display message: "Create a new project to get started"
+
+          ELSE (no projects)
+          Display message: "Create a new project to get started"
+
+          Show button: "+ Create New Project"
+          On click → call onStartAddProject()
+ */}
+    </div>
+  );
 };
 
 export default NoProject;
