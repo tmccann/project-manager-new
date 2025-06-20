c1f1c0528593ac312f9e5d06e8cd6b3e3f23deab
tommy <tommy.mccann001@gmail.com>
2025-06-20 17:50:03 +0100

    chore: add placeholder test files for each component to help track test coverage

---
+++ src/components/NoProject/NoProject.test.tsx
@@ -0,0 +1,4 @@
+test.todo("placeholder for NoProject component tests", () => {
+  // Test cases will be added later
+});
+// This file is a placeholder for future tests related to the NoProject component.
