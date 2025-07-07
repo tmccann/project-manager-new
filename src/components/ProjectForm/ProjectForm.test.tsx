076096fddbe0fbedd1acf87ebd0a7e9cb06a914b
Tommy <tommy.mccann001@gmail.com>
2025-07-07 10:50:24 +0100

    test: ProjectForm add tests for input handlers, onSubmit and Cancel (currently failing)

--- src/components/ProjectForm/ProjectForm.test.tsx
+++ src/components/ProjectForm/ProjectForm.test.tsx
@@ -80,11 +80,9 @@ describe("input validation", () => {
     expect(screen.getByText(errorMessages.dateError)).toBeInTheDocument();
   });
   test("inputs errors when user input invalid", async () => {
-    await ProjectFormHelpers.actions.userInput(user, "Title", "123");
     await ProjectFormHelpers.actions.userInput(user, "Description", "123");
     await ProjectFormHelpers.actions.userInput(user, "Due Date", "1/15/2025");
     await ProjectFormHelpers.actions.clickButton(user, "Save");
-    screen.debug();
     expect(screen.getByText(errorMessages.titleError)).toBeInTheDocument();
     expect(
       screen.getByText(errorMessages.descriptionError)
@@ -114,5 +112,143 @@ describe("input validation", () => {
       inputStrings.dueDateString
     );
     expect(screen.queryByText(errorMessages.dateError)).not.toBeInTheDocument();
+  });
+});
+describe("form submition", () => {
+  beforeEach(() => {
+    render(<ProjectForm handleSubmit={mockSubmit} />);
+    vi.restoreAllMocks();
+  });
+  test("does not call onSubmit if title input invalid", async () => {
+    await ProjectFormHelpers.actions.userInput(user, "Title", "123");
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Description",
+      inputStrings.descriptionString
+    );
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Due Date",
+      inputStrings.dueDateString
+    );
+    await ProjectFormHelpers.actions.clickButton(user, "Save");
+    expect(mockSubmit).not.toHaveBeenCalled();
+  });
+  test("does not call onSubmit if description input invalid", async () => {
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Title",
+      inputStrings.titleString
+    );
+    await ProjectFormHelpers.actions.userInput(user, "Description", "123");
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Due Date",
+      inputStrings.dueDateString
+    );
+    await ProjectFormHelpers.actions.clickButton(user, "Save");
+    expect(mockSubmit).not.toHaveBeenCalled();
+  });
+  test("does not call onSubmit if dueDate input invalid", async () => {
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Title",
+      inputStrings.titleString
+    );
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Description",
+      inputStrings.descriptionString
+    );
+    await ProjectFormHelpers.actions.userInput(user, "Due Date", "01/04/24");
+    await ProjectFormHelpers.actions.clickButton(user, "Save");
+    expect(mockSubmit).not.toHaveBeenCalled();
+  });
+  test("onSubmit called with correct values when inputs have valid values", async () => {
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Title",
+      inputStrings.titleString
+    );
+    expect(
+      screen.queryByText(errorMessages.titleError)
+    ).not.toBeInTheDocument();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Description",
+      inputStrings.descriptionString
+    );
+    expect(
+      screen.queryByText(errorMessages.descriptionError)
+    ).not.toBeInTheDocument();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Due Date",
+      inputStrings.dueDateString
+    );
+    await ProjectFormHelpers.actions.clickButton(user, "Save");
+    expect(mockSubmit).toBeCalledWith({
+      title: "New Project",
+      description: "Description goes here",
+      dueDate: today,
+    });
+  });
+  test("user inputs clear when onSubmit called", async () => {
+    const { titleInput, descriptionInput, dueDateInput } =
+      ProjectFormHelpers.getElements();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Title",
+      inputStrings.titleString
+    );
+    expect(
+      screen.queryByText(errorMessages.titleError)
+    ).not.toBeInTheDocument();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Description",
+      inputStrings.descriptionString
+    );
+    expect(
+      screen.queryByText(errorMessages.descriptionError)
+    ).not.toBeInTheDocument();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Due Date",
+      inputStrings.dueDateString
+    );
+    await ProjectFormHelpers.actions.clickButton(user, "Save");
+    expect(titleInput).toHaveValue("");
+    expect(descriptionInput).toHaveValue("");
+    expect(dueDateInput).toHaveValue("");
+  });
+  test("user inputs clear when cancel button clicked", async () => {
+    const { titleInput, descriptionInput, dueDateInput } =
+      ProjectFormHelpers.getElements();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Title",
+      inputStrings.titleString
+    );
+    expect(
+      screen.queryByText(errorMessages.titleError)
+    ).not.toBeInTheDocument();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Description",
+      inputStrings.descriptionString
+    );
+    expect(
+      screen.queryByText(errorMessages.descriptionError)
+    ).not.toBeInTheDocument();
+    await ProjectFormHelpers.actions.userInput(
+      user,
+      "Due Date",
+      inputStrings.dueDateString
+    );
+    await ProjectFormHelpers.actions.clickButton(user, "Cancel");
+    expect(titleInput).toHaveValue("");
+    expect(descriptionInput).toHaveValue("");
+    expect(dueDateInput).toHaveValue("");
   });
 });
