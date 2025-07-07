71ae6b6ff146aad91d194b443ee08a17d8b5353e
Tommy <tommy.mccann001@gmail.com>
2025-07-07 09:36:13 +0100

    feat: add input validation logic and error display to ProjectForm

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -1,29 +1,78 @@
 import React, { useRef, useState } from "react";
 
 type ProjectFormProps = {
-  handleSubmit: () => void;
-  // ProjectFormData: ProjectFormData;
+  handleSubmit: (data: ProjectFormData) => void;
 };
 
-type ProjectFormData = {
+export type ProjectFormData = {
   title: string;
   description: string;
   dueDate: string;
 };
-const initialProjectFormData = {
-  title: "",
-  description: "",
-  dueDate: "",
+const intialErrors = {
+  title: false,
+  description: false,
+  dueDate: false,
 };
 const ProjectForm = ({ handleSubmit }: ProjectFormProps) => {
+  const [errors, setErrors] = useState(intialErrors);
   // create ref for title, description and dueDateInput
-  const title = useRef(null);
-  const description = useRef(null);
-  const dueDate = useRef(null);
+  const title = useRef<HTMLInputElement>(null);
+  const description = useRef<HTMLInputElement>(null);
+  const dueDate = useRef<HTMLInputElement>(null);
 
+  // create validation for inputs
+  const onValidText = (ref: React.RefObject<HTMLInputElement>) => {
+    const textElement = ref.current;
+    if (!textElement) return null;
+    const val = textElement.value;
+    return val.length >= 4 ? val : null;
+  };
+  const onValidateText = (ref: React.RefObject<HTMLInputElement>) => {
+    const dateElement = ref.current;
+    if (!dateElement) return null;
+    const val = dateElement.value;
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const enteredDate = new Date(val);
+    console.log(enteredDate >= today ? val : null);
+    return enteredDate >= today ? val : null;
+  };
+
+  const handleFormValidation = (e: React.FormEvent) => {
+    e.preventDefault();
+    const userTitle = onValidText(title);
+    const userDescription = onValidText(description);
+    const userDueDate = onValidateText(dueDate);
+
+    if (!userTitle || !userDescription || !userDueDate) {
+      const newErrors = {
+        title: !userTitle,
+        description: !userDescription,
+        dueDate: !userDueDate,
+      };
+      setErrors(newErrors);
+    } else {
+      if (userTitle && userDescription && userDueDate) {
+        const data: ProjectFormData = {
+          title: userTitle,
+          description: userDescription,
+          dueDate: userDueDate,
+        };
+
+        // send data to app using handle submit
+        console.log(data);
+      }
+    }
+  };
+
+  // clear ref when data has valid submit
+  // clear data on cancel
+
+  const hasErrors = Object.values(errors).some(Boolean);
   return (
     <section>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleFormValidation}>
         <div>
           <button type="button">Cancel</button>
           <button type="submit">Save</button>
@@ -41,8 +90,27 @@ const ProjectForm = ({ handleSubmit }: ProjectFormProps) => {
         />
         {/*due date */}
         <label htmlFor="dueDate">Due Date</label>
-        <input type="text" name="dueDate" id="dueDate" ref={dueDate} />
+        <input
+          type="string"
+          name="dueDate"
+          id="dueDate"
+          ref={dueDate}
+          placeholder="mm/dd/yyyy"
+        />
       </form>
+      {/* create visual display if error */}
+      {hasErrors && (
+        <div>
+          <p>submited project form has the following errors</p>
+          {errors.title && (
+            <p> Title must be atleast 4 character not including spaces</p>
+          )}
+          {errors.description && (
+            <p>Description must be atleast 4 character not including spaces</p>
+          )}
+          {errors.dueDate && <p> Date must be today or later</p>}
+        </div>
+      )}
     </section>
   );
 };
