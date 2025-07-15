f3b765472878cab8ed40f972145ab96aa3806dc7
Tommy <tommy.mccann001@gmail.com>
2025-07-15 19:00:14 +0100

    feat: add SelectProject scaffolding and renamed button from "Clear" to "Delete"(tests now pass)

--- src/components/ProjectDisplayPage/SelectedProject/SelectedProject.tsx
+++ src/components/ProjectDisplayPage/SelectedProject/SelectedProject.tsx
@@ -1,14 +1,32 @@
 export type ProjectData = {
+  id: string;
   title: string;
   description: string;
   dueDate: string;
 };
 export type SelectedProjectProps = {
   projectData: ProjectData;
-  handleDelete: () => void;
+  handleDelete: (id: string) => void;
 };
-const SelectedProject = ({ projectData }: SelectedProjectProps) => {
-  return <div>SelectedProject</div>;
+
+const SelectedProject = ({
+  projectData,
+  handleDelete,
+}: SelectedProjectProps) => {
+  return (
+    <header>
+      <div>
+        {/* display title and delete button */}
+        {/* delete button should pass project id*/}
+        <h1>{projectData.title}</h1>
+        <button onClick={() => handleDelete(projectData.id)}>Delete</button>
+      </div>
+      {/* display due date */}
+      <p>{projectData.dueDate}</p>
+      {/* display discription  */}
+      <p>{projectData.description}</p>
+    </header>
+  );
 };
 
 export default SelectedProject;
